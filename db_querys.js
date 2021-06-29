1537155e6d4f434fc16e3042d9838f6fb285d58c
SamuelCHICHA <samuel.chicha.live.fr>
2021-06-29 22:18:07 +0200

    build: handle wrong conversion on data

--- db_querys.js
+++ db_querys.js
@@ -15,16 +15,17 @@ const pool = mariadb.createPool({
 /**
  * Let's define code for the response
  * 0 => Everything's good
- * 1 => Database communication failed
- * 2 => Data already exists (Case of insertion)
- * 3 => No match (case of selection)
- * 4 => Unknown
+ * 1 => Database error
+ * 2 => No match (case of selection)
+ * 3 => Unknown
+ * 4 =>
  */
 
 async function get_activities (guild_id){
     let response = {};
-    if(!(typeof guild_id ===  "number"))
-        throw new TypeError("Wrong types");
+    guild_id = parseInt(guild_id)
+    if(isNaN(guild_id))
+        throw new TypeError(`The guild id given couldn't be converted into an integer. guild_id: ${guild_id}.`);
     await pool.query("SELECT name FROM Activities WHERE guild_id=?", [guild_id])
         .then(dbr => {
             response.content = dbr.slice()
@@ -36,17 +37,18 @@ async function get_activities (guild_id){
             }
         })
         .catch(err => {
-            response.status = 1;
-            response.content = "Database Error";
-            console.error(err);
+            throw err
         });
     return response;
 }
 
 async function get_voice_channel_names(guild_id, activity){
     let response = {};
-    if(!(typeof guild_id === "number" && typeof activity === "string"))
-        throw new TypeError("Wrong types");
+    guild_id = parseInt(guild_id);
+    if(isNaN(guild_id))
+        throw new TypeError(`The guild id given couldn't be converted into an integer. guild_id: ${guild_id}.`);
+    if(!(typeof activity === "string"))
+        throw new TypeError("The activity is not a string.");
     await pool.query("SELECT Channels.name FROM ((Activities INNER JOIN Guilds on Activities.guild_id = Guilds.id and Guilds.id = ?) INNER JOIN Channels on Channels.activity_id = Activities.id and Activities.name = ?)", [guild_id, activity])
         .then(dbr => {
             response.content = dbr.slice();
@@ -58,17 +60,18 @@ async function get_voice_channel_names(guild_id, activity){
             }
         })
         .catch(err => {
-            response.status = 1;
-            response.content = "Database Error";
-            console.error(err);
+            throw err;
         });
     return response;
 }
 
 async function get_voice_channel_name(guild_id, activity){
     let response = {};
-    if(!(typeof guild_id === "number" && typeof activity === "string"))
-        throw new TypeError("Wrong types");
+    guild_id = parseInt(guild_id);
+    if(isNaN(guild_id))
+        throw new TypeError(`The guild id given couldn't be converted into an integer. guild_id: ${guild_id}.`);
+    if(!(typeof activity === "string"))
+        throw new TypeError("The activity is not a string.");
     await pool.query("SELECT Channels.name FROM ((Activities INNER JOIN Guilds ON Activities.guild_id = Guilds.id AND Guilds.id = ?) INNER JOIN Channels ON Channels.activity_id = Activities.id and Activities.name = ?) ORDER BY RAND() LIMIT 1", [guild_id, activity])
         .then(dbr => {
             if (dbr.length !== 0){
@@ -80,9 +83,7 @@ async function get_voice_channel_name(guild_id, activity){
             }
         })
         .catch(err => {
-            response.status = 1;
-            response.content = "Database Error"
-            console.error(err);
+            throw err
         });
     return response;
 }
@@ -91,17 +92,20 @@ async function register_activities(guild_id, activities) {
     let response = {};
     let i;
     let data = [];
-    if(!(typeof guild_id === "number" && Array.isArray(activities)))
-        throw new TypeError("Wrong types");
+    guild_id = parseInt(guild_id);
+    if(isNaN(guild_id))
+        throw new TypeError(`The guild id given couldn't be converted into an integer. guild_id: ${guild_id}.`);
+    if(!(Array.isArray(activities)))
+        throw new TypeError("Activities is not in an array.");
     for(i = 0; i < activities.length; i++)
         data.push([activities[i], guild_id]);
     await pool.batch("INSERT IGNORE INTO Activities(name, guild_id) VALUES(?, ?)", data)
-        .then(() => {
+        .then(dbr => {
             response.status = 0;
+            response.content = dbr["affectedRows"];
         })
         .catch(err => {
-            console.error(err);
-            response.status = 1;
+            throw err;
         });
     return response;
 }
@@ -112,16 +116,23 @@ async function register_channel_names(guild_id, activity, channel_names){
     let response = {};
     let data = []
     let sql_query;
-    if(!(typeof guild_id === "number" && typeof activity === "string" && Array.isArray(channel_names)))
-        throw new TypeError("Wrong types");
+    guild_id = parseInt(guild_id);
+    if(isNaN(guild_id))
+        throw new TypeError(`The guild id given couldn't be converted into an integer. guild_id: ${guild_id}.`);
+    if(!(typeof activity === "string"))
+        throw new TypeError("Activity is not a string.");
+    if(!Array.isArray(channel_names))
+        throw new TypeError("The channel names are not in an array.");
     for(i = 0; i < channel_names.length; i++)
         data.push([channel_names[i], activity, guild_id]);
     sql_query = "INSERT IGNORE INTO Channels(name, activity_id) VALUES (?, (SELECT id FROM Activities WHERE name=? AND guild_id=?))"
     await pool.batch(sql_query, data)
-        .then(() => response.status = 0)
+        .then(dbr => {
+            response.status = 0;
+            response.content = dbr["affectedRows"];
+        })
         .catch(err => {
-            console.error(err);
-            response.status = 1;
+            throw err;
         });
     return response;
 }
@@ -129,20 +140,16 @@ async function register_channel_names(guild_id, activity, channel_names){
 
 async function register_guild(guild_id){
     let response = {};
-    if(!(typeof guild_id === "number"))
-        throw new TypeError("Wrong type");
-    await pool.query("INSERT INTO Guilds VALUES (?)", [guild_id])
+    guild_id = parseInt(guild_id);
+    if(isNaN(guild_id))
+        throw new TypeError(`The guild id given couldn't be converted into an integer. guild_id: ${guild_id}.`);
+    await pool.query("INSERT IGNORE INTO Guilds VALUES (?)", [guild_id])
         .then(dbr => {
-            if(dbr["affectedRows"] === 1)
-                response.status = 0;
-            else {
-                console.warn(dbr);
-                response.status = 4;
-            }
+            response.status = 0;
+            response.content = dbr["affectedRows"];
         })
         .catch(err => {
-            console.error(err);
-            response.status = 1;
+            throw err;
         });
     return response;
 }
@@ -151,28 +158,36 @@ async function delete_activities(guild_id, activities){
     let response = {};
     let data = []
     let i;
-    if (!(typeof guild_id === "number" && typeof Array.isArray(activities)))
-        throw new TypeError("Wrong types");
+    guild_id = parseInt(guild_id);
+    if(isNaN(guild_id))
+        throw new TypeError(`The guild id given couldn't be converted into an integer. guild_id: ${guild_id}.`);
+    if (!(typeof Array.isArray(activities)))
+        throw new TypeError("Activities are not in an array.");
     for(i = 0; i < activities.length; i++)
         data.push([guild_id, activities[i]]);
     await pool.batch("DELETE FROM Activities WHERE guild_id=? AND name=?", data)
-        .then(() => response.status = 0)
+        .then(dbr => {
+            response.status = 0
+            response.content = dbr["affectedRows"];
+        })
         .catch(err => {
-            console.error(err);
-            response.status = 1;
+            throw err;
         });
     return response;
 }
 
 async function delete_guild(guild_id){
     let response = {};
-    if (!(typeof guild_id === "number"))
-        throw new TypeError("Wrong type");
+    guild_id = parseInt(guild_id);
+    if(isNaN(guild_id))
+        throw new TypeError(`The guild id given couldn't be converted into an integer. guild_id: ${guild_id}.`);
     await pool.query("DELETE FROM Guilds WHERE id=?", [guild_id])
-        .then(()=> response.status = 0)
+        .then(dbr => {
+            response.status = 0;
+            response.content = dbr["affectedRows"];
+        })
         .catch(err => {
-            console.error(err);
-            response.status = 1;
+            throw err;
         });
     return response;
 }
--- index.js
+++ index.js
@@ -2,36 +2,44 @@
  * Framework expressjs
  * https://expressjs.com/
  */
-const express = require('express')
-const app = express()
-app.use(express.json())
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 /**
  * Read a JSON file and parse it
  */
 const fs = require('fs')
-const ip_filtering_config_file = fs.readFileSync("ip_filtering_options.json")
-const ip_filtering_config = JSON.parse(String(ip_filtering_config_file))
+const ip_filtering_config_file = fs.readFileSync("ip_filtering_options.json");
+const ip_filtering_config = JSON.parse(String(ip_filtering_config_file));
 
 
-app.use((req, res, next) => {
-    console.log(`url: ${req.url}, params: ${req.params}`);
-    next();
-});
+
 
 /**
  * Middleware: checks that the ip address matches one of the authorized ones
  * https://github.com/tychovbh/expressjs-ip-control#readme
  */
-const access_control = require('express-ip-access-control')
-app.use(access_control(ip_filtering_config))
+const access_control = require('express-ip-access-control');
+
+app.use(access_control(ip_filtering_config));
+app.use(express.json());
 
 /**
  *
  */
-const db_handler = require('./db_querys')
+const db_handler = require('./db_querys');
 
+
+app.use((req, res, next) => {
+    let param;
+    let message = `url: ${req.url}, post-parameters: {`;
+    for(param in Object.values(req.body))
+        message += `${param},`;
+    message += "}"
+    console.log(message);
+    next();
+});
 
 /**
  * ROUTES
@@ -41,86 +49,134 @@ const db_handler = require('./db_querys')
  * Get a random channel name according to an activity in a guild.
  */
 app.get('/guild/:guild/activity/:activity/random-channel-name', (req, res) => {
-    let guild_id = parseInt(req.params["guild"]);
+    let guild_id = req.params["guild"];
     let activity = req.params["activity"];
     db_handler.get_voice_channel_name(guild_id, activity)
         .then(response => res.json(response))
-        .catch(response => res.json(response));
+        .catch(err => {
+            console.error(err);
+            res.json({
+                status: 1,
+                content: "Database error"
+            });
+        });
 });
 
 /**
  * Get list of channel names according to an activity in a guild.
  */
 app.get('/guild/:guild/activity/:activity/channel-names', (req, res) => {
-    let guild_id = parseInt(req.params["guild"]);
+    let guild_id = req.params["guild"];
     let activity = req.params["activity"];
     db_handler.get_voice_channel_names(guild_id, activity)
         .then(response => res.json(response))
-        .catch(response => res.json(response));
+        .catch(err => {
+            console.error(err);
+            res.json({
+                status: 1,
+                content: "Database error"
+            });
+        });
 });
 
 /**
  * Get the list of activities for a guild.
  */
 app.get('/guild/:guild/activities', (req, res) => {
-    let guild_id = parseInt(req.params["guild"]);
+    let guild_id = req.params["guild"];
     db_handler.get_activities(guild_id)
         .then(response => res.json(response))
-        .catch(response => res.json(response));
+        .catch(err => {
+            console.error(err);
+            res.json({
+                status: 1,
+                content: "Database error"
+            });
+        });
 });
 
 /**
  * Register new activities for the guild
  */
 app.post('/guild/:guild/register-activity', (req, res) => {
-    let guild_id = parseInt(req.params["guild"]);
+    let guild_id = req.params["guild"];
     let activities = req.body["activities"];
     db_handler.register_activities(guild_id, activities)
         .then(response => res.json(response))
-        .catch(response => res.json(response));
+        .catch(err => {
+            console.error(err);
+            res.json({
+                status: 1,
+                content: "Database error"
+            });
+        });
 });
 
 /**
  * Register new channel voice names
  */
 app.post("/guild/:guild/activity/:activity/register-channel-name", (req, res) => {
-    let guild_id = parseInt(req.params["guild"]);
+    let guild_id = req.params["guild"];
     let activity = req.params["activity"];
     let voice_channel_names = req.body["channel_names"];
     db_handler.register_channel_names(guild_id, activity, voice_channel_names)
         .then(response => res.json(response))
-        .catch(response => res.json(response));
+        .catch(err => {
+            console.error(err);
+            res.json({
+                status: 1,
+                content: "Database error"
+            });
+        });
 });
 
 /**
  * Register new guild
  */
 app.post("/register-guild", (req, res) => {
-   let guild_id = parseInt(req.body["guild_id"]);
-    db_handler.register_guild(guild_id)
-        .then(response => res.json(response))
-        .catch(response => res.json(response));
+   let guild_id = req.body["guild_id"];
+   db_handler.register_guild(guild_id)
+       .then(response => res.json(response))
+       .catch(err => {
+           console.error(err);
+           res.json({
+               status: 1,
+               content: "Database error"
+           });
+       });
 });
 
 /**
  * Delete a guild
  */
 app.post("/delete-guild", (req, res) => {
-    let guild_id = parseInt(req.body["guild_id"]);
+    let guild_id = req.body["guild_id"];
     db_handler.delete_guild(guild_id)
         .then(response => res.json(response))
-        .catch(response => res.json(response));
+        .catch(err => {
+            console.error(err);
+            res.json({
+                status: 1,
+                content: "Database error"
+            });
+        });
 });
 
 /**
  * Remove an activity from a guild
  */
 app.post("/guild/:guild/delete-activities", (req, res) => {
-    let guild_id = parseInt(req.params["guild"]);
-    let activity = req.body["activities"];
-    db_handler.delete_activities(guild_id, activity)
+    let guild_id = req.params["guild"];
+    let activities = req.body["activities"];
+    db_handler.delete_activities(guild_id, activities)
         .then(response => res.json(response))
-        .catch(response => res.json(response));
+        .catch(err => {
+        console.error(err);
+        res.json({
+            status: 1,
+            content: "Database error"
+        });
+    });
 });
 
 app.listen(port)
